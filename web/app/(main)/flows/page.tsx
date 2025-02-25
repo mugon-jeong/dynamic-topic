84d6e3cda60f194b08be924938a3de8a0bd7d130
conny <conny@musma.net>
2025-02-25 15:54:47 +0900

    flow list page에 생성 버튼 추가

--- web/app/(main)/flows/page.tsx
+++ web/app/(main)/flows/page.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import Link from "next/link";
 import {getFlows} from "@/app/(main)/flows/_actions/flow-action";
 import WorkFlow from "@/app/(main)/flows/_components/work-flow";
+import {Button} from "@/components/ui/button";
+import {Plus} from "lucide-react";
+import Link from "next/link";
+import {v4} from "uuid";
 
 const Page = async () => {
   const flows = await getFlows()
   console.log(flows)
   return (
       <div className="relative flex flex-col">
+        <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+          Workflows
+          <Button size={'icon'}>
+            <Link href={`/flows/${v4()}`}>
+              <Plus/>
+            </Link>
+          </Button>
+        </h1>
         <div className="relative flex flex-col gap-4">
           <section className="m-2 flex flex-col">
             {flows.map((flow) => (
